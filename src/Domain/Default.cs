8b420527104cd7192a8b0e40bb70296e9cf972a6
Dmitrii Bessonov <Dmitrii_Bessonov@epam.com>
2021-02-06 14:16:42 +0300

    add IFn

---
+++ src/Domain/Default.cs
@@ -0,0 +1,8 @@
+﻿namespace Sandbox.Number
+{
+    public readonly struct Default<T> : IFn<T, T>,  IFn<T, T, T>
+    {
+        public T Invoke(T in1) => default;
+        public T Invoke(T in1, T in2) => default;
+    }
+}
---
+++ src/Domain/IFn.cs
@@ -0,0 +1,12 @@
+﻿namespace Sandbox.Number
+{
+    public interface IFn<TIn, TOut>
+    {
+        TOut Invoke(TIn @in);
+    }
+
+    public interface IFn<TIn1, TIn2, TOut>
+    {
+        TOut Invoke(TIn1 in1, TIn2 in2);
+    }
+}
